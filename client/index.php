80c2497755e5d8d722409215c9f452cd139e6a78
Clarence Cho <clarencemindpalace@gmail.com>
2024-02-15 02:20:57 +0800

    Update index.php with css from jake

--- client/index.php
+++ client/index.php
@@ -6,9 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Page - Portfolio Management System</title>
-
-    <!-- CSS FILES -->
+    <title>Home Page - Portfolio Management System</title>
     <link rel="stylesheet" href="css/index.css">
 </head>
 
@@ -19,11 +17,79 @@ include("header.php");
 
 <body>
 
+<div class="photo-card">
+  <img src="images/placeholder.jpeg" alt="Placeholder Image">
+  <div class="card-content">
+    <h2>Name</h2>
+    <p>Description</p>
+  </div>
+</div>
+
+<div class="desc-card">
+  <div class="desc-content">
+    <h1>Create The Portfolio You Desire</h1>
+    <p>Tell us about yourself. Put yourself out there by selling your
+	strengths and skills.</p>
+	<p>Sign up today to create your very own portfolio to jumpstart
+	your dream career.</p>
+  </div>
+</div>
+
+<button class="my-button">Get Started</button>
+
+<h3>Latest Content</h3>
+
+<div class="card-container">
+  <div class="photo-card2">
+    <img src="images/placeholder.jpeg" alt="Placeholder Image">
+    <div class="card-content2">
+      <p>Description 1</p>
+    </div>
+  </div>
+  <div class="photo-card2">
+    <img src="images/placeholder.jpeg" alt="Placeholder Image">
+    <div class="card-content2">
+      <p>Description 2</p>
+    </div>
+  </div>
+  <div class="photo-card2">
+    <img src="images/placeholder.jpeg" alt="Placeholder Image">
+    <div class="card-content2">
+      <p>Description 3</p>
+    </div>
+  </div>
+</div>
+
+<body>
+  <div class="button-container">
+    <button class="prev-button"> < </button>
+    <button class="next-button"> > </button>
+  </div>
+
+<h4>Start Making Your Own Portfolio</h4>
+
+<div class="photo-card3">
+  <img src="images/placeholder.jpeg" alt="Placeholder Image">
+  <div class="card-content3">
+    <h2>Name</h2>
+    <p>Description</p>
+  </div>
+</div>
+
+<div class="desc-card3">
+  <div class="desc-content3">
+    <h1>Design Your Own Personal Portfolio</h1>
+    <p>Design your own portfolio with a minimalist and streamlined design.</p>
+	<p>Get hired by your desired company simply by showing off your skillsets and background.</p>
+  </div>
+</div>
+
+<button class="my-button2">Get Started</button>
+  
 </body>
 
-<!-- THIS IS HOW YOU CAN INCLUDE FILES SO YOU CAN REUSE THEM (FOOTER) -->
 <?php
-//include("footer.php");
+include("footer.php");
 ?>
 
 </html>
