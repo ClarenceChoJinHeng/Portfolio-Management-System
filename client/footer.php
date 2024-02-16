284c2bc63f61d9ae189a97299b54832571285382
Clarence Cho <clarencemindpalace@gmail.com>
2024-02-16 12:05:52 +0800

    Added footer.php/css

--- client/footer.php
+++ client/footer.php
@@ -6,11 +6,86 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- ====== LINK CSS ====== -->
+  <link rel="stylesheet" href="css/footer.css"/>
+
+  <!-- ====== LINK BOX ICON ====== -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
   <title>Footer Page - Portfolio Management System</title>
 </head>
 
 <body>
-  
+<footer>
+<div class="footer__container">
+
+<!-- ======= PORTFOLIO FOOTER TITLE ======== -->
+<div class="port__contact container">
+    <div class="title__container">
+      <div class="label__container"> 
+        <a href="index.php" class="footer__link">Portfolio</a>
+        <i class='bx bxs-report'></i>  
+      </div>
+
+     <div class="label__description">
+        <p class="nav__description">
+          Transforming the landscape of finding a  job and the right person to hire.
+        </p>
+     </div>
+</div>
+
+<!-- ======== CONTACT INFORMATION ========= -->
+  <div class="contact__container">
+        <label class="contact__label">
+         <u>Contact Information</u> 
+        </label>
+
+        <div class="contact__information">
+          <i class='bx bxs-phone'></i>
+          <span class="phone__number">(777) 333-4555</span>
+        </div>
+
+        <div class="contact__information">
+          <i class='bx bxs-envelope'></i>
+          <span class="email">info@portfolio.com</span>
+        </div>
+
+        <div class="contact__information">
+          <i class='bx bxs-location-plus'></i>
+          <span class="location">123 Main Street Cityville,
+            State<br> 54321 Countryland</span>
+        </div>
+   </div>
+</div>
+
+   <!-- ======== NAV LINK ======== -->
+   <div class="nav__social container">
+      <nav class="footer__nav">
+          <label class="nav__label"><u>Navigation Links</u></label>
+
+          <div class="nav__container">
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="faq.php">FAQs</a>
+          </div>
+      </nav>
+
+   <!-- ======== SOCIAL LINKS ========= -->
+   <div class="links__container">
+        <label class="social__title"><u>Social Links</u></label>
+
+      <div class="social__links">
+          <a href="" class="instagram"><i class='bx bxl-instagram-alt'></i></a>
+
+          <a href="" class="facebook"><i class='bx bxl-facebook-square' ></i></a>
+
+          <a href="" class="linkedin"><i class='bx bxl-linkedin-square'></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+  </footer>
 </body>
 
 </html>
