60d4d953dfcbdb43a1e60f65a19f1e1c2a7f93f4
Clarence Cho <clarencemindpalace@gmail.com>
2024-02-29 14:34:31 +0800

    Change connection to procedural

--- server/connection.php
+++ server/connection.php
@@ -6,7 +6,7 @@ $password = "";
 $dbname = "portfolio";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
